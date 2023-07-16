10e11e70530d443c8d426910b517b1a111ba6b2b
Cristhian Melo <rxtsel@hotmail.com>
2023-07-16 17:13:00 -0500

    chore: add images to swiper

--- src/components/main/Slider.jsx
+++ src/components/main/Slider.jsx
@@ -3,6 +3,31 @@ import { Carousel } from "react-responsive-carousel";
 
 import slider1Small from "../../assets/images/hero/slider/slider1-small.webp";
 import slider1Full from "../../assets/images/hero/slider/slider1-full.webp";
+import slider2Small from "../../assets/images/hero/slider/slider2-small.webp";
+import slider2Full from "../../assets/images/hero/slider/slider2-full.webp";
+import slider3Small from "../../assets/images/hero/slider/slider3-small.webp";
+import slider3Full from "../../assets/images/hero/slider/slider3-full.webp";
+import slider4Small from "../../assets/images/hero/slider/slider4-small.webp";
+import slider4Full from "../../assets/images/hero/slider/slider4-full.webp";
+
+const images = [
+  {
+    full: slider1Full,
+    small: slider1Small,
+  },
+  {
+    full: slider2Full,
+    small: slider2Small,
+  },
+  {
+    full: slider3Full,
+    small: slider3Small,
+  },
+  {
+    full: slider4Full,
+    small: slider4Small,
+  },
+];
 
 export function Slider() {
   return (
@@ -17,23 +42,16 @@ export function Slider() {
       interval={3500}
       showStatus={false}
     >
-      <picture className="slider-picture">
-        <source media="(max-width: 767px)" srcset={slider1Small} />
-        <source media="(min-width: 768px)" srcset={slider1Full} />
-        <img
-          src={slider1Full}
-          alt="puede ser una imagen de una obra de arte de isarte"
-        />
-      </picture>
-
-      <picture className="slider-picture">
-        <source media="(max-width: 767px)" srcset={slider1Small} />
-        <source media="(min-width: 768px)" srcset={slider1Full} />
-        <img
-          src={slider1Full}
-          alt="puede ser una imagen de una obra de arte de isarte"
-        />
-      </picture>
+      {images.map(({ full, small }) => (
+        <picture className="slider-picture">
+          <source media="(max-width: 767px)" srcset={small} />
+          <source media="(min-width: 768px)" srcset={full} />
+          <img
+            src={slider1Full}
+            alt="puede ser una imagen de una obra de arte de isarte"
+          />
+        </picture>
+      ))}
     </Carousel>
   );
 }
